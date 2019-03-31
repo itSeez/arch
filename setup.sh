2be782ceec1992b824a9bc59035a8938dd162202
Jose Alarcon <alarconh.jose@gmail.com>
2019-03-31 14:31:50 -0400

    more setup script updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # errors should fail the script
 set -e
 
@@ -17,8 +16,6 @@ chset()
 }
 
 # installation script
-
-# define some user directories
 echo "creating user directories"
 mkdir $HOME/bin # user binaries that should be in the path
 mkdir $HOME/src # source that is tracked by git
@@ -26,7 +23,6 @@ mkdir $HOME/wrk # source that is not tracked by git
 mkdir $HOME/pkg # packages not installed by pacman
 mkdir $HOME/tmp # temporary files
 
-# change system settings
 echo "applying system settings"
 chset "#Color" "Color" /etc/pacman.conf
 chset "#HandleSuspendKey=suspend" "HandleSuspendKey=ignore" /etc/systemd/logind.conf
@@ -38,35 +34,29 @@ chset "#AllowHibernation=yes" "AllowHibernation=no" /etc/systemd/sleep.conf
 chset "#AllowSuspendThenHibernate=yes" "AllowSuspendThenHibernate=no" /etc/systemd/sleep.conf
 chset "#AllowHybridSleep=yes" "AllowHybridSleep=no" /etc/systemd/sleep.conf
 
-# refresh arch keyring
 echo "refreshing arch keyring"
 pacman -Syy --noconfirm archlinux-keyring >/dev/null 2>&1
 
-# install arch packages
 curl -s "$pkgsfile" | sed '/^package/d' > $HOME/tmp/pkgs.csv
 while IFS=, read -r pkg comment; do
     echo "installing $pkg"
     pacman -S --noconfirm --needed "$pkg" >/dev/null 2>&1
 done < $HOME/tmp/pkgs.csv
 
-# install python packages
 echo "installing python packages"
 curl -s "$pypkgsfile" >> $HOME/tmp/pypkgs.txt
 python -m pip install --upgrade pip setuptools >/dev/null 2>&1
 python -m pip install -r $HOME/tmp/pypkgs.txt >/dev/null 2>&1
 
-# install sublime text
 echo "installing sublime text"
 curl -Os $sublkey >> $HOME/tmp/sublimehq-pub.gpg
 pacman-key --add $HOME/tmp/sublimehq-pub.gpg && pacman-key --lsign-key 8A8F901A
-echo -e "$sublrepo" | tee -a /etc/pacman.conf
+echo -e "$sublrepo" >> /etc/pacman.conf
 pacman -Syy --noconfirm sublime-text >/dev/null 2>&1
 
-# enable services
 echo "enabling services"
-timedatectl set-ntp true
-ufw enable
 systemctl enable ufw.service
+ufw enable
+timedatectl set-ntp true
 
-# all done
 echo "\ninstallation complete"
