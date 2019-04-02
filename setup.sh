a1a4eac3bf1878012edef41ca6523a1e840685f9
Jose Alarcon <alarconh.jose@gmail.com>
2019-04-01 20:12:43 -0400

    don't hide errors

--- setup.sh
+++ setup.sh
@@ -1,20 +1,15 @@
-#!/bin/sh
-# errors should fail the script
-set -e
-
+#!/bin/zsh
 # variables
 pkgsfile="https://bitbucket.org/itSeez/arch/raw/master/pkgs.csv"
-pypkgsfile="https://bitbucket.org/itSeez/arch/raw/master/pypkgs.txt"
 sublkey="https://download.sublimetext.com/sublimehq-pub.gpg"
 sublrepo="\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64"
 home_dir=/home/itseez
 
 # functions
-chset()
-{
-    # change the setting in file $3 from value $1 to value $2
-    sudo sed -i "s/^$1/$2/" $3
-}
+error() { printf "\nerror %s\n" "$1"; exit; }
+
+# change the setting in file $3 from value $1 to value $2
+chset() { sudo sed -i "s/^$1/$2/" $3 }
 
 # installation script
 echo "creating user directories"
@@ -35,34 +30,31 @@ chset "#AllowHibernation=yes" "AllowHibernation=no" /etc/systemd/sleep.conf
 chset "#AllowSuspendThenHibernate=yes" "AllowSuspendThenHibernate=no" /etc/systemd/sleep.conf
 chset "#AllowHybridSleep=yes" "AllowHybridSleep=no" /etc/systemd/sleep.conf
 
+# add sublime text to pacman
+curl -s "$sublkey" > $home_dir/tmp/subl.gpg
+sudo pacman-key --add $home_dir/tmp/subl.gpg > /dev/null || error "adding sublime-text"
+sudo pacman-key --lsign-key 8A8F901A > /dev/null || error "adding sublime-text"
+echo -e "$sublrepo" | sudo tee -a /etc/pacman.conf
+
 echo "refreshing arch keyring"
-sudo pacman -Syy --noconfirm archlinux-keyring >/dev/null 2>&1
+sudo pacman -Syy --noconfirm archlinux-keyring > /dev/null || error "refreshing arch keyring"
 
 echo "installing packages"
-curl -s "$pkgsfile" | sed '/^package/d' > $home_dir/tmp/pkgs.csv
-while IFS=, read -r pkg comment; do
-    echo -n "$pkg "
-    sudo pacman -S --noconfirm --needed "$pkg" >/dev/null 2>&1
+curl -s "$pkgsfile" > $home_dir/tmp/pkgs.csv
+while IFS=, read -r key pkg comment; do
+    printf "%s " "$pkg"
+    case "$key" in
+        "" ) sudo pacman -S --noconfirm --needed "$pkg" > /dev/null || error "installing $pkg" ;;
+        "p" ) sudo pip install "$pkg" > /dev/null || error "installing $pkg" ;;
+    esac
 done < $home_dir/tmp/pkgs.csv
 echo ""
-
-echo "installing python packages"
-curl -s "$pypkgsfile" > $home_dir/tmp/pypkgs.txt
-sudo python -m pip install --upgrade pip setuptools >/dev/null 2>&1
-sudo python -m pip install -r $home_dir/tmp/pypkgs.txt >/dev/null 2>&1
-
-echo "installing sublime text"
-curl -s "$sublkey" > $home_dir/tmp/subl.gpg
-sudo pacman-key --add $home_dir/tmp/subl.gpg >/dev/null 2>&1
-sudo pacman-key --lsign-key 8A8F901A >/dev/null 2>&1
-echo -e "$sublrepo" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syy --noconfirm sublime-text >/dev/null 2>&1
 
 echo "enabling services"
 sudo systemctl enable ufw.service
 sudo ufw enable
 
 echo "cleaning up temporary files"
-rm $home_dir/tmp/pkgs.csv $home_dir/tmp/pypkgs.txt $home_dir/tmp/subl.gpg
+rm $home_dir/tmp/pkgs.csv $home_dir/tmp/subl.gpg
 
-echo -e "\ninstallation complete"
+printf "\n%s\n" "installation complete"
