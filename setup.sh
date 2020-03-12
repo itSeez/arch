b15d5f291546118242e07adeacd2ec7b76c850e5
Jose Alarcon <alarconh.jose@gmail.com>
2020-03-12 07:45:02 -0400

    removed unreliable packages

--- setup.sh
+++ setup.sh
@@ -37,7 +37,6 @@ main()
 
     echo "enabling services"
     systemctl enable NetworkManager.service >> /dev/null 2>&1 || exit
-    systemctl enable org.cups.cupsd.socket >> /dev/null 2>&1 || exit
     systemctl enable bluetooth.service >> /dev/null 2>&1 || exit
     systemctl enable gdm.service >> /dev/null 2>&1 || exit
     systemctl enable ufw.service >> /dev/null 2>&1 || exit
